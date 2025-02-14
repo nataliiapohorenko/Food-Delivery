7d440212d98241670089084cee2a793ff318afc4
Nataliia Pohorenko <natasha.pogorenko@gmail.com>
2025-02-14 12:13:55 +0200

    feat: food-item page

--- src/app/pages/food-item-details/food-item-details.component.ts
+++ src/app/pages/food-item-details/food-item-details.component.ts
@@ -7,6 +7,12 @@ import { RatingWidgetComponent } from '../../shared/components/rating-widget/rat
 import { RoutingConstants } from '../../constants/routes.constants';
 import { LayoutService } from '../../services/layout.service';
 
+interface Addon {
+  name: string;
+  price: number;
+  countable: boolean;
+}
+
 @Component({
   selector: 'app-food-item-details',
   standalone: true,
@@ -20,12 +26,35 @@ import { LayoutService } from '../../services/layout.service';
 })
 export class FoodItemDetailsComponent implements OnInit {
   routingConstants = RoutingConstants;
+  itemCounts = new Map();
   foodItem: FoodItemInterface =
     inject(ActivatedRoute).snapshot.data['foodItem'];
   private layoutService: LayoutService = inject(LayoutService);
 
   ngOnInit(): void {
     this.setBackButtonLink();
+  }
+
+  increment(item: FoodItemInterface | Addon) {
+    const defaultValue = item === this.foodItem ? 1 : 0;
+    this.itemCounts.set(item, (this.itemCounts.get(item) ?? defaultValue) + 1);
+  }
+
+  decrement(item: FoodItemInterface | Addon) {
+    const currentCount = this.itemCounts.get(item) || 0;
+    if (currentCount > 1) {
+      this.itemCounts.set(item, currentCount - 1);
+    } else {
+      this.itemCounts.delete(item);
+    }
+  }
+
+  toggleAddon(addon: Addon) {
+    if (this.itemCounts.has(addon)) {
+      this.itemCounts.delete(addon);
+    } else {
+      this.itemCounts.set(addon, 1);
+    }
   }
 
   setBackButtonLink(): void {
